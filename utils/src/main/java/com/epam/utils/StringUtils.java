1093e93b32b5ec5ffd2c705b76bd347c0af8c679
Shapovalov Dmytro <buonviagio@gmail.com>
2023-04-05 11:11:33 +0300

    My sixth commit Stage2 Module#1 Task#2

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,17 +2,23 @@ package com.epam.utils;
 
 import org.apache.commons.lang3.math.NumberUtils;
 
+import java.math.BigInteger;
+
 public class StringUtils {
+
     public static boolean isPositiveNumber(String str) {
-        if (NumberUtils.isCreatable(str)){
-            if(str.endsWith("d") || str.endsWith("D")){
+        if (NumberUtils.isCreatable(str)) {
+            if (str.endsWith("d") || str.endsWith("D")) {
                 return NumberUtils.createDouble(str) > 0;
-            } else if(str.endsWith("l") || str.endsWith("L")){
+            } else if (str.endsWith("l") || str.endsWith("L")) {
                 return NumberUtils.createLong(str) > 0;
-            } else if(str.endsWith("f") || str.endsWith("F")){
+            } else if (str.endsWith("f") || str.endsWith("F")) {
                 return NumberUtils.createFloat(str) > 0;
             }
         }
-        return NumberUtils.toLong(str) > 0;
+        if (NumberUtils.isDigits(str)) {
+            return NumberUtils.createBigInteger(str).compareTo(new BigInteger("0")) > 0;
+        }
+        return false;
     }
 }
--- utils/src/test/java/com/epam/utils/StringUtilsTest.java
+++ utils/src/test/java/com/epam/utils/StringUtilsTest.java
@@ -8,9 +8,10 @@ class StringUtilsTest {
 
     @Test
     void isPositiveNumber() {
-        String s1 = "65", s2 = "-12", s3 = "F";
+        String s1 = "65", s2 = "-12", s3 = "F", s4 = "54d";
         assertTrue(StringUtils.isPositiveNumber(s1));
         assertFalse(StringUtils.isPositiveNumber(s2));
         assertFalse(StringUtils.isPositiveNumber(s3));
+        assertTrue(StringUtils.isPositiveNumber(s4));
     }
 }
