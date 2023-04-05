d22491a720bbe6b71b178c06f54395d16b3a584c
Shapovalov Dmytro <buonviagio@gmail.com>
2023-04-05 10:41:46 +0300

    My forth commit Stage2 Module#1 Task#2

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -4,6 +4,15 @@ import org.apache.commons.lang3.math.NumberUtils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
+        if (NumberUtils.isCreatable(str)){
+            if(str.endsWith("d") || str.endsWith("D")){
+                return NumberUtils.createDouble(str) > 0;
+            } else if(str.endsWith("l") || str.endsWith("L")){
+                return NumberUtils.createLong(str) > 0;
+            } else if(str.endsWith("f") || str.endsWith("F")){
+                return NumberUtils.createFloat(str) > 0;
+            }
+        }
         return NumberUtils.toInt(str) > 0;
     }
 }
